60ea0cc28659df290392978a31bda71d7c0f8143
akash <akasgangan2001@gmail.com>
2026-05-30 14:08:21 +0530

    Setup shadcn ui

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { backendStatus } from "@/services/api";
 import { useEffect, useState } from "react";
 
@@ -7,16 +8,17 @@ import { useEffect, useState } from "react";
 export default function Home() {
   const[message,setMessage]=useState("Loading....")
 
-  useEffect(()=>{
+    function click(){
     backendStatus()
     .then((data)=>setMessage(data.message))
     .catch(()=>setMessage("Backend connection failed"))
-  },[])
+    }
   return (
     <main className="flex min-h-screen items-center justify-center">
       <h1 className="text-4xl font-bold">
         AI PDF Chat Application
       </h1>
+      <Button onClick={click}>Click here</Button>
       <p>Backend Status: {message}</p>
     </main>
   );
